a40bad581fc9545a510bb2c48d5664b69746e57a
Hijas Ahamed <hijasahamed98@gmail.com>
2018-08-20 17:16:34 +1000

    Book.java
    I have changed some varibale names and class names in the java file from version 1.00 to version 1.12

--- book.java
+++ book.java
@@ -1,34 +1,35 @@
-package javaapplication4;
+
 
 import java.io.Serializable;
 
 
+
 @SuppressWarnings("serial")
-public class Book implements Serializable {   //book has changed to Book
+public class Book implements Serializable {   // version_1.00 changed class name first letter into capital
 	
-	private String Title;   // changed to Tilte
-	private String Author;  // changed to Author
-	private String CallNo;  // changed to CallNo
-	private int BookID;     // changed to BookID
+	private String title;   // version_1.01 T replaced to tilte
+	private String author;  // version_1.02 A replaced to author
+	private String callNo;  // version_1.03 C replaced to callNo
+	private int bookId;     // version_1.04 ID replaced to bookId
 	
 	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
 	private STATE state;
 	
 	
-	public Book(String author, String title, String callNo, int id) {  //book has changed to Book
-		this.Author = author;
-		this.Title= title;
-		this.CallNo = callNo;
-		this.BookID = id;
+	public Book(String author, String title, String callNo, int id) { //changed class name first letter into capital
+		this.author = author;   // version_1.05 A replaced to author
+		this.title= title;      // version_1.06 T replaced to tilte
+		this.callNo = callNo;   // version_1.07 C replaced to callNo
+		this.bookId = id;       // version_1.08 ID replaced to bookId
 		this.state = STATE.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(BookID).append("\n")
-		  .append("  Title:  ").append(Title).append("\n")
-		  .append("  Author: ").append(Author).append("\n")
-		  .append("  CallNo: ").append(CallNo).append("\n")
+		sb.append("Book: ").append(bookId).append("\n")      / version_1.09 ID replaced to bookId
+		  .append("  Title:  ").append(title).append("\n")   // version_1.10 T replaced to tilte
+		  .append("  Author: ").append(author).append("\n")  // version_1.11 A replaced to author
+		  .append("  CallNo: ").append(callNo).append("\n")  // version_1.12 C replaced to callNo
 		  .append("  State:  ").append(state);
 		
 		return sb.toString();
