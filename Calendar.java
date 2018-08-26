a9553ae5dcbaed4c6a550e1b9b632c713f4e70a2
Waqas Bajwa <wbajwa313@gmail.com>
2018-08-26 19:28:24 +1000

    Final Complete Calendar.java
    Final comments added

--- Calendar.java
+++ Calendar.java
@@ -21,7 +21,7 @@ public class Calendar {
 		return selfCal; 
 	}
 	
-	public void incrementDate(int incDays) { //updated by reviewer-Dushan - days to incDays
+	public void incrementDate(int incDays) { // version 4.0 final comment by author(waqas) variable days changed - updated by reviewer-Dushan - days to incDays
 		calendarNow.add(java.util.Calendar.DATE, incDays);		 //Variable name should be meaningful and camelCase
 	}
 	
@@ -39,11 +39,11 @@ public class Calendar {
 	}
 	public synchronized Date dateNow() { //updated by reviewer Dushan - method name must be camelBack
 		try {
-	        calendarNow.set(java.util.Calendar.HOUR_OF_DAY, 0);  //Variable name should be meaningful and camelCase
-	        calendarNow.set(java.util.Calendar.MINUTE, 0);  
-	        calendarNow.set(java.util.Calendar.SECOND, 0);  
-	        calendarNow.set(java.util.Calendar.MILLISECOND, 0);
-			return calendarNow.getTime();
+	        calendarNow.set(java.util.Calendar.HOUR_OF_DAY, 0);  //version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
+	        calendarNow.set(java.util.Calendar.MINUTE, 0);  //version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
+	        calendarNow.set(java.util.Calendar.SECOND, 0);  //version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
+	        calendarNow.set(java.util.Calendar.MILLISECOND, 0);//version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
+			return calendarNow.getTime(); //version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -52,14 +52,14 @@ public class Calendar {
 
 	public synchronized Date getDueDate(int loanPeriod) {
 		Date nowDate = Date();
-		calendarNow.add(java.util.Calendar.DATE, loanPeriod); //Variable name should be meaningful and camelCase
+		calendarNow.add(java.util.Calendar.DATE, loanPeriod); //version 4.0 update by author (Waqas)- Variable name should be meaningful and camelCase - variable name changed to calendarNow
 		Date dueDate = calendarNow.getTime();
 		calendarNow.setTime(now);
 		return dueDate;
 	}
 	
 	public synchronized long getDaysDifference(Date targetDate) {
-		long diffMilliSeconds = Date().getTime() - targetDate.getTime();  // Variable name should be meaningful diffMilliSeconds
+		long diffMilliSeconds = Date().getTime() - targetDate.getTime();  // Version 4.0 final update (Waqas)- Variable name should be meaningful diffMillis to diffMilliSeconds
 	    long diffDays = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS);
 	    return diffDays;
 	}
